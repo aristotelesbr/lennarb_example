09d3a81a862718149ec525a05cb6743584004fba
Aristóteles <aristotelesbr@gmail.com>
2024-07-08 11:03:31 -0300

    test GA setup

--- config/app.rb
+++ config/app.rb
@@ -2,15 +2,16 @@
 
 require 'lennarb'
 
+
 class App < Lennarb::Application::Base
   get '/' do |_req, res|
     res.status = 200
     res.html 'Hello, World!'
   end
 
+
   post '/' do |req, res|
     res.status = 200
     res.json  req.body
-
   end
 end
