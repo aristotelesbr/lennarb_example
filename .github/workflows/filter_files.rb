fda0c04c0246017e0bbd1c6c58bb085ab92b9770
Aristóteles <aristotelesbr@gmail.com>
2024-07-08 16:51:53 -0300

    --no-message

---
+++ .github/workflows/filter_files.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require 'fnmatch'
+
+# Load excluded paths from RuboCop configuration
+rubocop_config = YAML.load_file('.rubocop.yml')
+excluded_paths = rubocop_config['AllCops']['Exclude']
+puts "Excluded paths: #{excluded_paths}"
+
+# Get changed files
+changed_files = ENV['CHANGED_FILES'].split("\n")
+puts "Changed files: #{changed_files}"
+
+# Filter out excluded paths
+filtered_files =
+  changed_files.reject do |file|
+    excluded_paths.any? { |pattern| File.fnmatch(pattern, file, File::FNM_PATHNAME) }
+  end
+
+puts "Filtered files: #{filtered_files.join(' ')}"
+filtered_files_string = filtered_files.join(' ')
+puts "::set-output name=files::#{filtered_files_string}"
