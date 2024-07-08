23bbc1eb306e1b2cc0f95df9a81f9f67d80ac361
Aristóteles <aristotelesbr@gmail.com>
2024-07-08 10:06:17 -0300

    setup lint GHA

--- config/app.rb
+++ config/app.rb
@@ -9,4 +9,9 @@ class App < Lennarb::Application::Base
     res.status = 200
     res.html('Hello, World!')
   end
+
+  post '/' do |req, res|
+    res.status = 200
+    res.json req.body 
+  end
 end
