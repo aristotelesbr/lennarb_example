7f5cb1f10b761d6c5a0ac733fc48c50218a5c923
Aristóteles <aristotelesbr@gmail.com>
2024-07-08 17:37:37 -0300

    --no-message

--- config/app.rb
+++ config/app.rb
@@ -2,7 +2,6 @@
 
 require 'lennarb'
 
-
 class App < Lennarb::Application::Base
   get '/' do |_req, res|
     res.status = 200
@@ -11,6 +10,6 @@ class App < Lennarb::Application::Base
 
   post '/' do |req, res|
     res.status = 200
-    res.json  req.body
+    res.json req.body
   end
 end
