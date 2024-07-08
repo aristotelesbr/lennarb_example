b82426c825e92f6dd2e33d06a9f9b02172594de1
Aristóteles <aristotelesbr@gmail.com>
2024-07-08 17:34:48 -0300

    --no-message

--- .github/workflows/filter_files.rb
+++ .github/workflows/filter_files.rb
@@ -4,6 +4,9 @@ require 'yaml'
 
 # Load excluded paths from RuboCop configuration
 rubocop_config = YAML.load_file('.rubocop.yml')
+puts "=" * 80
+puts rubocop_config
+puts "=" * 80
 excluded_paths = rubocop_config['AllCops']['Exclude']
 puts "Excluded paths: #{excluded_paths}"
 
@@ -13,11 +16,12 @@ changed_files = changed_files.split("\n")
 puts "Changed files: #{changed_files}"
 
 # Filter out excluded paths
-filtered_files =
-  changed_files.reject do |file|
-    excluded_paths.any? { |pattern| File.fnmatch(pattern, file, File::FNM_PATHNAME | File::FNM_EXTGLOB) }
-  end
+filtered_files = changed_files.reject do |file|
+  match = excluded_paths.any? { |pattern| File.fnmatch(pattern, file, File::FNM_PATHNAME | File::FNM_EXTGLOB) }
+  puts "File: #{file}, Match: #{match}"
+  match
+end
 
 puts "Filtered files: #{filtered_files.join(' ')}"
 filtered_files_string = filtered_files.join(' ')
-File.open(ENV.fetch('GITHUB_ENV', nil), 'a') { |f| f.puts "FILTERED_FILES=#{filtered_files_string}" }
+File.open(ENV['GITHUB_ENV'], 'a') { |f| f.puts "FILTERED_FILES=#{filtered_files_string}" }
